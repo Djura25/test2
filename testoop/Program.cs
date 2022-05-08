a634c5f9c1638a081bcf68502ddf27a18d95384f
Milos Djurovic <milosdj.2004@gmail.com>
2022-05-08 23:39:52 +0200

    Prvi komit

---
+++ testoop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(":)");
+            Console.ReadKey();
+        }
+    }
+}
